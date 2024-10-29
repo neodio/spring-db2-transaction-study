4b53b255cb1af53cd9bfdda9dfdb3ec716dacc4d
장정우 <jungwoo@lotte.net>
2024-10-29 18:33:58 +0900

    first commit

---
+++ src/test/java/hello/springtx/SpringDb2TransactionStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDb2TransactionStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
